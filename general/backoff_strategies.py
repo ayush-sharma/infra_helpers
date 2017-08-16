95727d8f7f27f03cdd7d19d4d75c379e4611a6c9
ayushsharma <ayushsharma.22@gmail.com>
2017-08-16 21:52:05 +0530

    Updating code with more backoff strategies.

--- general/backoff_strategies.py
+++ general/backoff_strategies.py
@@ -4,68 +4,191 @@ from time import sleep
 def do_something():
     """ This is our testing function. As long as this function returns false, our retry functions below will keep
     retrying. """
-
+    
     return False
 
 
 def no_backoff(max_retry: int):
     """ No backoff. If things fail, retry immediately. """
-
+    
     counter = 0
     while counter < max_retry:
-
+        
         response = do_something()
         if response:
-
+            
             return True
         else:
-            print('> No Backoff > Retry number: %s' % str(counter))
-
+            # print('> No Backoff > Retry number: %s' % counter)
+            pass
+        
         counter += 1
+
+
+def constant_backoff(max_retry: int):
+    """ Constant backoff. If things fail, retry after a fixed amount of delay. """
+    
+    total_delay = 0
+    counter = 0
+    while counter < max_retry:
+        
+        response = do_something()
+        if response:
+            
+            return True
+        else:
+            
+            sleepy_time = 1
+            # print('> Constant Backoff > Retry number: %s, sleeping for %s seconds.' % (counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
+        counter += 1
+    
+    print('> Constant Backoff > Total delay: %s seconds.' % total_delay)
 
 
 def linear_backoff(max_retry: int):
     """ Linear backoff. If things fail, retry with linearly increasing delays. """
-
+    
+    total_delay = 0
     counter = 0
     while counter < max_retry:
-
+        
         response = do_something()
         if response:
-
+            
             return True
         else:
-
+            
             sleepy_time = counter
-            print('> Linear Backoff > Retry number: %s, sleeping for %s seconds.' % (str(counter), str(sleepy_time)))
-            # sleep(sleepy_time)
-
+            # print('> Linear Backoff > Retry number: %s, sleeping for %s seconds.' % (counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
         counter += 1
+    
+    print('> Linear Backoff > Total delay: %s seconds.' % total_delay)
+
+
+def get_fib(num: int):
+    """ Get sum of Fibonacci numbers up to given number. """
+    
+    if num <= 1:
+        
+        return num
+    else:
+        return get_fib(num - 1) + get_fib(num - 2)
+
+
+def fibonacci_backoff(max_retry: int):
+    """ Fibonacci backoff. If things fail, retry with delays increasing by fibonacci numbers. """
+    
+    total_delay = 0
+    counter = 0
+    while counter < max_retry:
+        
+        response = do_something()
+        if response:
+            
+            return True
+        else:
+            
+            sleepy_time = get_fib(counter)
+            # print(
+            #         '> Fibonacci Backoff > Retry number: %s, sleeping for %s seconds.' % (
+            #             counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
+        counter += 1
+    
+    print('> Fibonacci Backoff > Total delay: %s seconds.' % total_delay)
 
 
 def exponential_backoff(max_retry: int):
     """ Exponential backoff. If things fail, retry with exponentially increasing delays. """
-
+    
+    total_delay = 0
     counter = 0
     while counter < max_retry:
-
+        
         response = do_something()
         if response:
-
+            
             return True
         else:
-
-            sleepy_time = counter ** 2
-            print('> Exponential Backoff > Retry number: %s, sleeping for %s seconds.' % (str(counter), str(sleepy_time)))
-            # sleep(sleepy_time)
-
+            
+            sleepy_time = 2 ** counter
+            # print(
+            #         '> Exponential Backoff > Retry number: %s, sleeping for %s seconds.' % (
+            #             counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
         counter += 1
+    
+    print('> Exponential Backoff > Total delay: %s seconds.' % total_delay)
+
+
+def quadratic_backoff(max_retry: int):
+    """ Quadratic backoff. If things fail, retry with polynomially increasing delays. """
+    
+    total_delay = 0
+    counter = 0
+    while counter < max_retry:
+        
+        response = do_something()
+        if response:
+            
+            return True
+        else:
+            
+            sleepy_time = counter ** 2
+            # print(
+            #         '> Quadratic Backoff > Retry number: %s, sleeping for %s seconds.' % (
+            #             counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
+        counter += 1
+    
+    print('> Quadratic Backoff > Total delay: %s seconds.' % total_delay)
+
+
+def polynomial_backoff(max_retry: int):
+    """ Polynomial backoff. If things fail, retry with polynomially increasing delays. """
+    
+    total_delay = 0
+    counter = 0
+    while counter < max_retry:
+        
+        response = do_something()
+        if response:
+            
+            return True
+        else:
+            
+            sleepy_time = counter ** 3
+            # print(
+            #         '> Polynomial Backoff > Retry number: %s, sleeping for %s seconds.' % (
+            #             counter, sleepy_time))
+            total_delay += sleepy_time
+            sleep(sleepy_time)
+        
+        counter += 1
+    
+    print('> Polynomial Backoff > Total delay: %s seconds.' % total_delay)
 
 
 if __name__ == '__main__':
-    max_retries = 10
+    max_retries = 20
     print('Starting experiments with maximum %s retries.' % str(max_retries))
-
+    
     no_backoff(max_retry=max_retries)
+    constant_backoff(max_retry=max_retries)
     linear_backoff(max_retry=max_retries)
+    fibonacci_backoff(max_retry=max_retries)
+    quadratic_backoff(max_retry=max_retries)
     exponential_backoff(max_retry=max_retries)
+    polynomial_backoff(max_retry=max_retries)
